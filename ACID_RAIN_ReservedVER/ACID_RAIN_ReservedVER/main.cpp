56289637c5e8daef16529c242bae901984b6875b
guddus326 <guddus326@naver.com>
2020-10-05 15:20:40 +0900

    메뉴리스트 화살표 이동

--- ACID_RAIN_ReservedVER/ACID_RAIN_ReservedVER/main.cpp
+++ ACID_RAIN_ReservedVER/ACID_RAIN_ReservedVER/main.cpp
@@ -1,44 +1,104 @@
-#include < windows.h >     // windows.h를 인클루드 시킵니다.
+#include <Windows.h>
 #include <iostream>
+#include <conio.h>
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define SUBMIT 4
+
 using namespace std;
+
+void gotoxy(int, int);
+void printTitle();
+int printMenuList();
+int keyControl();
+void init();
 
 void gotoxy(int x, int y)
 {
 	COORD pos = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-
-void consoleSetting(struct _gameStatus* gameStatus) {
-	system("title Acid Rain");
-	system("mode con: cols=64 lines=10");
+void init() {
+	system("mode con cols=60 lines=40");
 }
-
 void printTitle() {
-
+	
 	cout << endl << endl << endl << endl;
-	printf("      #       ###   ###    ###       \n");
-	printf("     ##     ##       #     #  #      \n");
-	printf("    #  #   ##        #     #   #     \n");
-	printf("   #####    ##       #     #  #      \n");
-	printf("   #   #      ###   ###    ###       \n");
+	cout <<"      #       ###   ###    ###       \n";
+	cout << "     ##     ##       #     #  #      \n";
+	cout << "    #  #   ##        #     #   #     \n";
+	cout << "   #####    ##       #     #  #      \n";
+	cout << "   #   #      ###   ###    ###       \n";
 
 	cout << endl << endl;
-	printf("             ###         #    ###   ##   # \n");
-	printf("             #  #      #  #    #    # #  # \n");
-	printf("             ###      #####    #    #  # # \n");
-	printf("             #  #    #     #   #    #   ## \n");
-	printf("             #   #  #      #  ###   #    # \n");
+	cout << "             ###         #    ###   ##   # \n";
+	cout << "             #  #      #  #    #    # #  # \n";
+	cout << "             ###      #####    #    #  # # \n";
+	cout << "             #  #    #     #   #    #   ## \n";
+	cout << "             #   #  #      #  ###   #    # \n";
 	cout << endl << endl << endl;
 }
 
-void printMenuList() {
-	gotoxy(9, 18); cout << "회원";
-	gotoxy(9, 21); cout << "비회원"<<endl;
+int printMenuList() {
+	int x=20;
+	int y=20;
+	gotoxy(x, y); cout   <<"게임시작";
+	gotoxy(x, y+1); cout <<"게임정보"<<endl;
+	gotoxy(x, y+2); cout <<"게임종료" << endl;
+
+	while (1) {//무한반복
+		int n = keyControl();
+		switch (n) {
+			case UP: {
+				if (y > 20) {
+					gotoxy(x - 2, y);
+					cout << " ";
+					gotoxy(x - 2, --y);
+					cout << ">";
+				}
+				break;
+			}
+			case DOWN: {
+				if (y < 22) {
+					gotoxy(x - 2, y);
+					cout << " ";
+					gotoxy(x - 2, ++y);
+					cout << ">";
+				}break;
+			}
+			case SUBMIT: {
+				return y-12;
+			}
+		}
+	}
+}
+
+
+int keyControl() {
+	char temp = _getch();
+
+	if (temp == 'w' || temp == 'W') {
+		return UP;
+	}else if (temp == 'a' || temp == 'A') {
+		return LEFT;
+	}else if (temp == 'd' || temp == 'D') {
+		return RIGHT;
+	}else if (temp == 's' || temp == 'S') {
+		return DOWN;
+	}else if (temp == ' ') {
+		return SUBMIT;
+	}
+
 }
 
 int main() {
+	init();
 	printTitle();
 	printMenuList();
-
-	return 0;
+	
+	
+	
 }
